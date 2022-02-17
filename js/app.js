624da3a7d4da5e6b94a422f799ca948d916a9838
solayman1e <solaymanweb27@gmail.comm>
2022-02-17 18:30:24 +0600

    error handeling

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-
+//get all input value for function
 function getExpenses(products){
     const expensesInput = document.getElementById(products + '-input');
     const totalInputExpenses = expensesInput.value;
+    
+if(totalInputExpenses == ""){
+    alert('Opps ! Please fill up input box')
+    return 
+
+}else if(totalInputExpenses < 0){
+    alert('Opps ! Negetive Number Is Not Allow')
+
+}else{
     return totalInputExpenses;
 }
+}
 
-// function balance(){
-//     incomeInput =  getExpenses('income');
-   
-// }
-
+// calculate event handler
 document.getElementById('calculate-btn').addEventListener('click', function(){
 
     const totalIncome = getExpenses('income');
@@ -26,6 +32,38 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
    const balanceDisplay =  document.getElementById('balance-display')
    balanceDisplay.innerText = balance;
 
+})
+//save button event handelar
+document.getElementById('save').addEventListener('click', function(){
+
+    const totalIncomes = getExpenses('income');
+    const previousBalance = totalIncomes;
+   
+    const saveInput = document.getElementById('save-input')
+    const savePercentage = saveInput.value;
+
+    const totalPercentage = (previousBalance/100)* savePercentage;
+
+    const savingAmount = document.getElementById('saving-amount')
+    savingAmount.innerText =totalPercentage;
+    const totalSavingAmount= savingAmount.innerText
 
 
+    const balanceDisplay =  document.getElementById('balance-display');
+
+    const preBalance = balanceDisplay.innerText;
+
+    if(preBalance < totalPercentage){
+        alert(' You Owe ! You have no money for balance');
+        return;
+    }else{
+        const remaidarBalance = preBalance - totalSavingAmount;
+        document.getElementById('remaining-balance').innerText = remaidarBalance;
+    }
+
+
+
+    
+    
+    
 })
